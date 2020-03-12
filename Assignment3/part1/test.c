58bf42465b844ae1c4129ac8c0e9a2d649994902
SDTheSlayer <dalal.shivang@gmail.com>
2020-03-12 20:07:59 +0530

    Flex start

---
+++ Assignment3/part1/test.c
@@ -0,0 +1,27 @@
+#define EOI 0 /* End of input */
+#define SEMI 1 /* ; */
+/**/#define PLUS 2 /* + #define*/ // /* */
+#define TIMES 3 //
+#define LP 4   #define /*/**/ 
+#define RP 5 /* ) */
+#define NUM_OR_ID 6 /* Decimal Number or Identifier */ /**//**/
+extern char *yytext; /* in lex.c */
+extern int yyleng; 
+extern yylineno;
+// int x,
+//  y ;
+int main()
+{
+ /* statement_
+ 
+ #define
+  block */
+    char * * temp;
+ return 0;    
+ // "My name"
+}
+
+int temp()
+{
+
+} 
